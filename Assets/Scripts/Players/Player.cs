972f7e87f398a5ecc109b2abd6f3dda87e0a0ed5
jackdelahunt <jdelahun@redhat.com>
2021-10-09 20:41:29 +0100

    Feat: player object now is controlled by user

---
+++ Assets/Scripts/Players/Player.cs
@@ -0,0 +1,76 @@
+using System;
+using Tiles;
+using UnityEngine;
+using Worlds;
+
+namespace Players
+{
+    [RequireComponent(typeof(CharacterController))]
+    //[RequireComponent(typeof(Animator))]
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float jumpForce = 400f;
+        [SerializeField] private float movementSpeed = 10f;
+
+        private bool isPlayerIdle = true;
+
+
+        private CharacterController controller;
+        private Animator animator;
+        // private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+        // private static readonly int IsJumping = Animator.StringToHash("IsJumping");
+
+        private void Awake()
+        {
+            controller = GetComponent<CharacterController>();
+            //animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            isPlayerIdle = true;
+            CheckPlayerMovement();
+            CheckPlayerJump();
+            CheckPlayerIdle();
+        }
+
+        /*
+         * checks for player movement an invokes the
+         * player movement event or
+         */
+        private void CheckPlayerMovement()
+        {
+            if (Input.GetButton("Horizontal"))
+            {
+                isPlayerIdle = false;
+                controller.Move(Input.GetAxisRaw("Horizontal") * movementSpeed);
+            }
+        }
+
+        /*
+         * checks for player movement an invokes the
+         * player movement event or
+         */
+        private void CheckPlayerJump()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                isPlayerIdle = false;
+                controller.Jump(jumpForce);
+            }
+        }
+
+        /*
+         * checks if the the player has moved or jumped
+         * this frame
+         */
+        private void CheckPlayerIdle()
+        {
+            if (isPlayerIdle)
+            {
+                // animator.SetBool(IsWalking, false);
+                // animator.SetBool(IsJumping, false);
+            }
+        }
+    }
+}
