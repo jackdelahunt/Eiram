e4fc44996667bd77bcb73783609c2d8085192efa
Jack Delahunt <20085409@mail.wit.ie>
2022-01-07 04:47:49 +0000

    Feat: added building animation UI

---
+++ Assets/Scripts/Inventories/BuildingUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Events;
+using UnityEngine;
+
+namespace Inventories
+{
+    public class BuildingUI : MonoBehaviour
+    {
+        private bool toggled = false;
+        
+        public void Awake()
+        {
+            EiramEvents.PlayerToggleInventoryEvent += OnPlayerToggleInventoryEvent;
+
+        }
+
+        public void Start()
+        {
+            
+        }
+        
+        private void OnDestroy()
+        {
+            EiramEvents.PlayerToggleInventoryEvent -= OnPlayerToggleInventoryEvent;
+        }
+
+        private void OnPlayerToggleInventoryEvent(PlayerInventory playerInventory)
+        {
+            if(toggled) CloseInventory(); else OpenInventory();
+            toggled = !toggled;
+        }
+        
+        private void OpenInventory()
+        {
+            LeanTween.moveX(gameObject, gameObject.transform.position.x - 50.0f, 0.4f);
+        }
+        
+        private void CloseInventory()
+        {
+            LeanTween.moveX(gameObject, gameObject.transform.position.x + 50.0f, 0.4f);
+        }
+    }
+}
