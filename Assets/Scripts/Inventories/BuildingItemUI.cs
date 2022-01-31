6994b02afa8b6710e085605484197b34d4b2af5f
Jack Delahunt <20085409@mail.wit.ie>
2022-01-31 23:15:54 +0000

    Feat: building items now how recipes

--- Assets/Scripts/Inventories/BuildingItemUI.cs
+++ Assets/Scripts/Inventories/BuildingItemUI.cs
@@ -3,15 +3,19 @@ using Eiram;
 using Recipes;
 using Registers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Inventories
 {
-    public class BuildingItemUI : MonoBehaviour
+    public class BuildingItemUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         private Image iconImage;
         private Action<BuildingRecipe> callback;
         private BuildingRecipe recipe;
+        [SerializeField] private GameObject hoverCard;
+        [SerializeField] private GameObject countableItemPrefab = null;
+        [SerializeField] private ScrollableListUI requirementsList;
 
         public void Awake()
         {
@@ -28,6 +32,24 @@ namespace Inventories
             this.callback = callback;
             this.recipe = recipe;
             this.iconImage.sprite = Register.GetItemByItemId(recipe.FinalItem.ItemId).Sprite();
+            
+            foreach (var itemCountPair in recipe.Ingredients)
+            {
+                var icon = requirementsList.Add(countableItemPrefab);
+                var countableItem = icon.GetComponent<CountableItem>();
+                countableItem.Image.sprite = Register.GetItemByItemId(itemCountPair.ItemId).Sprite();
+                countableItem.Count.text = itemCountPair.Amount.ToString();
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            hoverCard.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            hoverCard.SetActive(false);
         }
     }
 }
