9e6c9a0341cdb106379b3af039aa6a7c8db3ad15
Jack Delahunt <20085409@mail.wit.ie>
2022-02-06 18:43:52 +0000

    Feat: can now set resolution in option menu

--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Menus
@@ -8,10 +10,36 @@ namespace Menus
     {
         [SerializeField] private GameObject MainMenu = null;
         [SerializeField] private GameObject StartMenu = null;
+        [SerializeField] private TMP_Dropdown ResolutionDropdown = null;
+        [SerializeField] private TMP_Dropdown ScreenOptionsDropdown = null;
+
+        public void OnEnable()
+        {
+            ResolutionDropdown.options.Clear();
+            foreach (var resolution in Screen.resolutions)
+            {
+                ResolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolution.width + " x " + resolution.height));
+            }
+            
+        }
+        
+        public void ApplyClicked()
+        {
+            var selectedResolution = Screen.resolutions[ResolutionDropdown.value];
+            Screen.SetResolution(selectedResolution.width, selectedResolution.height, 
+                ScreenOptionsDropdown.value switch
+                {
+                    0 => FullScreenMode.ExclusiveFullScreen,
+                    1 => FullScreenMode.FullScreenWindow,
+                    2 => FullScreenMode.Windowed,
+                    _ => FullScreenMode.ExclusiveFullScreen
+                }
+                );
+        }
 
         public void BackClicked()
         {
-            MainMenu.SetActive(true);
+            MainMenu.SetActive(true);   
             StartMenu.SetActive(false);
             gameObject.SetActive(false);
         }
